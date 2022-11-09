c3211db63bcdfcadd240d1c639894e0d24465bf4
lilla-k <kalmarlilianna@gmail.com>
2022-11-09 12:02:45 +0100

    show more and less

--- script.js
+++ script.js
@@ -5,16 +5,24 @@ inputElement.id = "search-input";
 inputElement.placeholder = "Search";
 const loadingDiv = document.createElement("div");
 rootElement.appendChild(loadingDiv);
-loadingDiv.id="loading-div";
-loadingDiv.textContent="Loading...";
+loadingDiv.id = "loading-div";
+loadingDiv.textContent = "Loading...";
 const cardsContainer = document.createElement("div");
 rootElement.appendChild(cardsContainer);
-cardsContainer.id="grid-container";
+cardsContainer.id = "grid-container";
+const filteredName = inputElement.value
 
-async function getData(){
+// inputElement.addEventListener("change", {
+//     const response = await fetch("https://api.github.com/users");
+//     const data = await response.json();
+// })
+
+
+
+async function getData() {
     const response = await fetch("https://api.github.com/users");
     const data = await response.json();
-    loadingDiv.style.display="none";
+    loadingDiv.style.display = "none";
     console.log(data)
     data.map(obj => {
         const userCard = document.createElement("div");
@@ -22,22 +30,49 @@ async function getData(){
         userCard.setAttribute("class", "card");
         const img = document.createElement("img");
         userCard.appendChild(img);
-        img.src=obj.avatar_url;
+        img.src = obj.avatar_url;
         const nameElement = document.createElement("div");
         userCard.appendChild(nameElement);
         nameElement.setAttribute("class", "name");
-        nameElement.textContent=obj.login;
+        nameElement.textContent = obj.login;
         const buttonElement = document.createElement("button");
         buttonElement.setAttribute("class", "show-button");
         userCard.appendChild(buttonElement);
         buttonElement.textContent = "Show more";
+        const rankElement = document.createElement("div");
+        userCard.appendChild(rankElement);
+        rankElement.textContent = "Rank: User";
+        rankElement.style.display = "none";
+        const adminElement = document.createElement("div");
+        userCard.appendChild(adminElement);
+        adminElement.textContent = "Admin: False";
+        adminElement.style.display = "none";
+
+
+        buttonElement.addEventListener("click", function onShowButton() {
+
+            if (buttonElement.textContent === "Show more") {
+                buttonElement.textContent = "Show less";
+                rankElement.style.display = "block";
+                adminElement.style.display = "block";
+            }
+            else if (buttonElement.textContent === "Show less") {
+                buttonElement.textContent = "Show more";
+                rankElement.remove();
+                adminElement.remove();
+                rankElement.style.display = "none";
+                adminElement.style.display = "none";
+            }
+        })
+
+
 
 
     })
-    
+
 }
 
-function init(){
+function init() {
     getData()
 }
 
